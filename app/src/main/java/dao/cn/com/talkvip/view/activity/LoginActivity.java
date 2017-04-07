4cdead98c51a0c6c648f0356739191ec2804a0d7
xjpqlf <xjpqlf@163.com>
2017-04-07 13:42:24 +0800

    第二天工程

--- app/src/main/java/dao/cn/com/talkvip/view/activity/LoginActivity.java
+++ app/src/main/java/dao/cn/com/talkvip/view/activity/LoginActivity.java
@@ -1,9 +1,28 @@
 package dao.cn.com.talkvip.view.activity;
 
-import android.support.design.widget.TextInputLayout;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import dao.cn.com.talkvip.Constants;
 import dao.cn.com.talkvip.R;
+import dao.cn.com.talkvip.utils.DebugFlags;
+import dao.cn.com.talkvip.utils.RsaU;
+import dao.cn.com.talkvip.utils.SPUtils;
+import dao.cn.com.talkvip.utils.ToastUtil;
+import okhttp3.Call;
 
 /**
  * @name dao.cn.com.talkvip.view.activity
@@ -18,9 +37,12 @@ import dao.cn.com.talkvip.R;
 
 public class LoginActivity extends BaseActivity {
 
-    private TextInputLayout mInputLayout1;
-    private TextInputLayout mInputLayout2;
-    private EditText mMember;
+
+    private EditText mEtac;
+    private EditText mEtpwd;
+    private TextView mTverroac;
+    private TextView mTverropwd;
+    private RelativeLayout mRl;
 
     @Override
     protected void initHead() {
@@ -34,11 +56,108 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        mInputLayout1 = (TextInputLayout) findViewById(R.id.textInputLayout1);
-        mInputLayout2 = (TextInputLayout) findViewById(R.id.textInputLayout2);
+        mEtac = (EditText) findViewById(R.id.tv_loginac);
+        mEtpwd = (EditText) findViewById(R.id.tv_logpwd);
 
-        mMember = mInputLayout1 .getEditText();
+        mTverroac = (TextView) findViewById(R.id.tv_erroac);
+        mTverropwd = (TextView) findViewById(R.id.tv_erropwd);
+        mRl = (RelativeLayout) findViewById(R.id.bt_login);
+        mRl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
 
+
+    }
+
+
+    private void Login() {
+
+        final String ac = mEtac.getText().toString();
+        final String pwd = mEtpwd.getText().toString();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date curDate = new Date(System.currentTimeMillis());
+        String str = formatter.format(curDate);
+        String sign = ac + "|" + pwd + "|" + str;
+        String strs = RsaU.encryptByPublic(sign);
+
+
+        if (TextUtils.isEmpty(ac) || TextUtils.isEmpty(pwd)) {
+
+            ToastUtil.showInCenter("密码账号不能为空");
+            return;
+        }
+
+        OkHttpUtils.post()
+                .url(Constants.BASE_URL + "/login/loginOn")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+
+                .addParams("sign", strs)
+
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        DebugFlags.logD("失败" + e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        DebugFlags.logD("登录" + response);
+
+                        try {
+                            JSONObject json = new JSONObject(response);
+
+                            String result = json.getString("result");
+                            String code = json.getString("code");
+                            String data = json.getString("data");
+
+
+                            if ("8888".equals(code)) {
+                                JSONObject json1 = new JSONObject(data);
+
+                                String token = json1.getString("token");
+                                String name = json1.getString("realname");
+                                Constants.TOKEN = token;
+                                SPUtils.putString(LoginActivity.this,"ac",ac);
+                                SPUtils.putString(LoginActivity.this,"pwd",pwd);
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("name", name);
+                                startActivity(intent);
+
+                            } else if ("1001".equals(code)) {
+                                mTverropwd.setText("");
+                                mTverroac.setText("用户名有误");
+
+
+                            } else if ("1002".equals(code)) {
+                                mTverroac.setText("");
+                                mTverropwd.setText("密码有误");
+                            } else if ("1003".equals(code)) {
+
+                                mTverropwd.setText("");
+                                mTverroac.setText("账号和密码有误");
+
+                            } else if ("1004".equals(code)) {
+                                mTverropwd.setText("");
+                                mTverroac.setText("账号不存在");
+
+                            } else if ("1005".equals(code)) {
+                                mTverropwd.setText("");
+                                mTverroac.setText("账号已关闭");
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
 
 
     }
---
+++ app/src/main/java/dao/cn/com/talkvip/view/activity/WelcomeActivity.java
@@ -0,0 +1,214 @@
+package dao.cn.com.talkvip.view.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import dao.cn.com.talkvip.Constants;
+import dao.cn.com.talkvip.R;
+import dao.cn.com.talkvip.utils.DebugFlags;
+import dao.cn.com.talkvip.utils.RsaU;
+import dao.cn.com.talkvip.utils.SPUtils;
+import okhttp3.Call;
+
+/**
+ * Created by uway on 2017/4/5.
+ */
+public class WelcomeActivity extends BaseActivity {
+    static Handler handler = new Handler();
+    private ImageView iv;
+
+
+    @Override
+    protected void initHead() {
+
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_welcome;
+    }
+
+    @Override
+    protected void initView() {
+        iv = (ImageView) findViewById(R.id.welcome_iv);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        playAnimation();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // 取消将要执行的任务
+        handler.removeCallbacks(goNextUiRunnable);
+
+    }
+
+    private void playAnimation() {
+        // 创建动画集合
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.setAnimationListener(animationListener);
+        // 构造缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        scaleAnimation.setDuration(1000);
+
+        // 构造透明度动画
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+
+        alphaAnimation.setDuration(2000);
+
+        // 把两个动画都添加到集合中
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(alphaAnimation);
+
+        // 一起播放
+        iv.startAnimation(animationSet);
+
+    }
+
+
+    private Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
+        // 动画播放完成后，会调用此方法
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            // 过一会跳转
+            handler.postDelayed(goNextUiRunnable, 1000);
+        }
+    };
+
+    Runnable goNextUiRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            goNextUi();
+        }
+
+    };
+
+    private void goNextUi() {
+       String ac=SPUtils.getString(WelcomeActivity.this,"ac","");
+       String pwd=SPUtils.getString(WelcomeActivity.this,"pwd","");
+          if (!TextUtils.isEmpty(ac)&&!TextUtils.isEmpty(pwd)){
+
+             Wlogin(ac,pwd);
+
+
+          }else {
+
+
+              Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+
+              startActivity(intent);
+              finish();
+
+
+
+          }
+
+
+
+        }
+
+    private void Wlogin(String ac,String pwd) {
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date curDate = new Date(System.currentTimeMillis());
+        String str = formatter.format(curDate);
+        String sign = ac + "|" + pwd + "|" + str;
+        String strs = RsaU.encryptByPublic(sign);
+
+
+
+
+        OkHttpUtils.post()
+                .url(Constants.BASE_URL + "/login/loginOn")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+
+                .addParams("sign", strs)
+
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        DebugFlags.logD("失败" + e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        DebugFlags.logD("欢迎" + response);
+
+                        try {
+                            JSONObject json = new JSONObject(response);
+
+                            String result = json.getString("result");
+                            String code = json.getString("code");
+                            String data = json.getString("data");
+                            JSONObject json1 = new JSONObject(data);
+
+                            String token = json1.getString("token");
+                            String name = json1.getString("realname");
+                            Constants.TOKEN = token;
+                            if ("8888".equals(code)) {
+
+                                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                                intent.putExtra("name", name);
+                                startActivity(intent);
+                                finish();
+
+                            } else{
+
+                                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+
+                                startActivity(intent);
+                                finish();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+
+
+    }
+
+
+}
